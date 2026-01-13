8aa7b5095c6ca26280adc1e8c0ebcd5bdb966c29
devtailor <devtailor81@gmail.com>
2026-01-13 23:22:03 +0530

    feat: advanced useState - learned state management with objects and spread operator

---
+++ 08-useState-Advance/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { use, useState } from 'react'
+
+const App = () => {
+  const [num, setnum] = useState({user: 'Dev', age: 22})
+    const btnClicked = () => {
+      const newnum = {...num}
+      newnum.user = 'Pihu'
+      newnum.age = 23
+      setnum(newnum)
+    }
+  
+  return (
+    <div>
+      <h1>{num.user}, {num.age}</h1>
+      <button onClick={btnClicked}>click</button>
+    </div>
+  )
+} 
+
+export default App
