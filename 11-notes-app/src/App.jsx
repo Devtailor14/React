8527d353a015c8ded8f51494da7640d448b0206e
devtailor <devtailor81@gmail.com>
2026-01-20 00:29:19 +0530

    Notes app completed with Delete functionality

--- 11-notes-app/src/App.jsx
+++ 11-notes-app/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+
 
 const App = () => {
 
@@ -10,13 +11,19 @@ const App = () => {
   const submitHandler = (event) => {
     event.preventDefault()
 
-    const copyTasks = [...tasks] 
+    const copyTasks = [...tasks]
     copyTasks.push({ title, details })
     setTasks(copyTasks)
 
- 
+
     setTitle('')
     setDetails('')
+  }
+
+  const deleteNote = (index) => {
+    const copyTasks = [...tasks]
+    copyTasks.splice(index, 1)
+    setTasks(copyTasks)
   }
 
   return (
@@ -45,14 +52,18 @@ const App = () => {
       </form>
       <div className=' lg:border-l-2 lg:w-1/2 p-10'>
         <h1 className='text-4xl font-bold'>Recent Notes</h1>
-        <div className='flex flex-wrap items-start justify-start gap-5 mt-5 h-full overflow-auto'>
+        <div className='flex flex-wrap items-start justify-start gap-5 mt-5 h-[90%] overflow-auto'>
           {tasks.map((task, index) => (
-            <div key={index} className='relative rounded-2xl text-black py-9 px-4 bg-cover bg-[url(https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png)] h-52 w-40'>
-              
-              <h3 className='font-bold text-xl leading-tight'>{task.title}</h3>
-              <p className='mt-2 leading-tight font-medium text-gray-500'>{task.details}</p>
+            <div key={index} className='flex justify-between flex-col items-center relative rounded-2xl text-black py-9  pb-4 px-4 bg-cover bg-[url(https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png)] h-52 w-40'>
+              <div>
+                <h3 className='font-bold text-lg leading-tight'>{task.title}</h3>
+                <p className='mt-2 leading-tight text-xs font-semibold text-gray-600'>{task.details}</p>
+              </div>
+              <button onClick={() => deleteNote(index)} className='bg-red-500 cursor-pointer active:scale-95 font-bold text-white outline-none py-1 w-full rounded'>Delete</button>
+
             </div>
-          )) }
+
+          ))}
 
         </div>
 
